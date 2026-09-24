fb628639e38ca593d22c7bb3c120f660c005c5a6
anuj-tiwari-iphtech <anuj-tiwari@iphtechnologies.com>
2026-09-24 18:04:10 +0530

    menu add to cart card

---
+++ src/Checkouts/MenuCard.jsx
@@ -0,0 +1,31 @@
+import '../components/FoodCard.css'
+
+export default function MenuCard({
+  title,
+  ingredients = [],
+  image,
+  onAddtoCart,
+  price,
+}) {
+  return (
+    <div className="food-card-wrapper">
+      <div className="food-card-image-container">
+        <img src={image} alt={title} className="food-card-image" />
+      </div>
+
+      <div className="food-card-body">
+        <h3 className="food-card-title">{title}</h3>
+
+        <p className="food-card-ingredients">
+          {ingredients.join(" • ")}
+        </p>
+        <div className='menu-card-price'>
+          <h3>${price}</h3>
+          <button className="food-card-btn" onClick={onAddtoCart}>
+            Add to cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
